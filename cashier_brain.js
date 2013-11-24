daa2d8f8f77af03f60346c8c2af3e2844b1d3092
Tony Arnold Stephen <stephentony5@gmail.com>
2013-11-24 11:44:03 +0000

    added script tag to index.html added button in index.html for new_item function added a new shopping list array in cashier_brain.js added function add_item to cashier_brain.js which pushes shopping_list into array added alert in cashier_brain.js to add_item, which alerts when added item. added function new_item which adds item.name and item.price to shopping list

--- cashier_brain.js
+++ cashier_brain.js
@@ -1,1 +1,15 @@
-//Cashier Reciept Calculator JS
+//cashier reciept calculator js
+
+var shopping_list = []; //creates a new shopping_list array
+
+function add_item(item){ 
+     shopping_list.push(item); //pushes shopping_list into array 
+     alert(item.name + ' added '); //alert item added
+}
+
+function new_item(){
+     var item = {}; //create item
+     item.name = prompt("name of item",""); //enter name of item
+     item.price = prompt("price of Item(in pounds)",""); //enter price of item
+     add_item(item); //add new item to shopping list
+}
