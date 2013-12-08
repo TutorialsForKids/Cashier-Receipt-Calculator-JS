2bb75291ce4baa1e4892abf63b1880d158a67b88
Omor Hussain <omor.hussain@gmail.com>
2013-12-08 11:47:46 +0000

    update

--- cashier_brain.js
+++ cashier_brain.js
@@ -4,6 +4,7 @@ var shopping_list = []; //creates a new shopping_list array
 function add_item(item){
 	shopping_list.push(item); //pushes shopping_list into array
 	alert(item.name + 'added!'); //alert item added
+	show_receipt();
 
 }
 
@@ -16,3 +17,25 @@ function new_item(){
 	add_item(item); //add new item to shopping list
 
 }
+
+ function show_receipt(){
+
+ 	var receipt_text = "";
+ 	var total = 0;
+
+ 	for (i = 0; i < shopping_list.length; i++){
+
+ 		var item = shopping_list[i];
+ 		var totalPrice = item.totalPrice;
+
+ 		receipt_text = receipt_text + item.name +" "
+ 		+ item.quantity + "@&#163;" + item.price
+ 		+ " each. Total = &#163;" + item.totalPrice + "<br/>";
+
+ 		alert(total);
+ 		total = total + item.totalPrice;  
+ 	}
+ 		receipt_text = receipt_text + "Final Price " + total;
+
+ 	document.getElementById('receipt_display').innerHTML = receipt_text;
+ }
